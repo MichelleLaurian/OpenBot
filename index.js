da1299e3747f95117919d6fb97cff38cb1aa9f41
Michelle <michellelaurian@gmail.com>
2022-08-10 12:22:29 -0300

    Este es mi primer proyecto de Javascript con Node en Open Bootcamp

---
+++ index.js
@@ -0,0 +1,9 @@
+/** POO
+ * clases: en un esquema o mdedlo de donde seguir
+ * objetos: es una instancia de la clase(una copia de la clase)
+ * los objetos tienen: 
+ *  ATRIUTOS: es una caracteristica o propiedad del objeto(variables dentro del un onbjeto)
+ *  METODOS: sonllas acciones que un objeto puede realizar
+ */
+
+ console.log("vamos que tenemos que seguir");
